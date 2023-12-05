5c7d1da7b40f2589f1e85e73ecfb75206428538e
Johan Lindh <johan@linkdata.se>
2023-12-05 08:41:05 +0100

    doc

--- cacher.go
+++ cacher.go
@@ -8,9 +8,14 @@ import (
 
 type Cacher interface {
 	// DnsSet may store a copy of msg with dns.Msg.Zero set to true and return it later with DnsGet.
+	// If nsaddr is invalid, the entry may be returned for any later query matching just the
+	// query name and type (a wildcard entry).
 	DnsSet(nsaddr netip.Addr, msg *dns.Msg)
 
-	// DnsGet returns the cached dns.Msg for the given qname and qtype. If nsaddr is valid, only cached responses
-	// from that server are returned. dns.Msg.Zero must be set to true to indicate response is served from cache.
+	// DnsGet returns the cached dns.Msg for the given qname and qtype that was returned from the
+	// server at nsaddr. If that is not in the cache, a wildcard entry with an invalid address
+	// may be returned if it exists. If no matching responses are available, nil is returned.
+	//
+	// dns.Msg.Zero must be set to true to indicate response is served from cache.
 	DnsGet(nsaddr netip.Addr, qname string, qtype uint16) (netip.Addr, *dns.Msg)
 }
