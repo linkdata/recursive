571f96312dec2360f0d57e4f00d9d123842ebf84
Johan Lindh <johan@linkdata.se>
2024-04-24 07:28:38 +0200

    rename Standard to CachedResolver

--- standard.go
+++ standard.go
@@ -8,15 +8,15 @@ import (
 	"golang.org/x/net/proxy"
 )
 
-type Standard struct {
+type CachedResolver struct {
 	proxy.ContextDialer
 	*Recursive
 	Cacher
 }
 
-var DefaultStandard = NewStandard(nil, nil, nil)
+var DefaultCachedResolver = NewCachedResolver(nil, nil, nil)
 
-func NewStandard(rec *Recursive, cd proxy.ContextDialer, cacher Cacher) CachingResolver {
+func NewCachedResolver(rec *Recursive, cd proxy.ContextDialer, cacher Cacher) CachingResolver {
 	if rec == nil {
 		rec = New()
 	}
@@ -26,13 +26,17 @@ func NewStandard(rec *Recursive, cd proxy.ContextDialer, cacher Cacher) CachingR
 	if cacher == nil {
 		cacher = DefaultCache
 	}
-	return &Standard{
+	return &CachedResolver{
 		ContextDialer: cd,
 		Recursive:     rec,
 		Cacher:        cacher,
 	}
 }
 
-func (std *Standard) DnsResolve(ctx context.Context, qname string, qtype uint16) (msg *dns.Msg, srv netip.Addr, err error) {
+func (std *CachedResolver) DnsResolve(ctx context.Context, qname string, qtype uint16) (msg *dns.Msg, srv netip.Addr, err error) {
 	return std.Recursive.ResolveWithOptions(ctx, std, std, nil, qname, qtype)
 }
+
+/*LookupHost(ctx context.Context, host string) (addrs []string, err error)
+LookupIPAddr(ctx context.Context, host string) ([]IPAddr, error)
+LookupNetIP(ctx context.Context, network, host string) ([]netip.Addr, error)*/
