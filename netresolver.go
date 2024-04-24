6e222456572d4cb8bc3790291b116455acfa7632
Johan Lindh <johan@linkdata.se>
2024-04-24 10:49:04 +0200

    fix naming

--- netresolver.go
+++ netresolver.go
@@ -3,6 +3,7 @@ package recursive
 import (
 	"context"
 	"net"
+	"net/netip"
 
 	"github.com/miekg/dns"
 )
@@ -24,7 +25,7 @@ func (std *Recursive) lookupNetIP(ctx context.Context, ips []net.IP, host string
 	return ips, err
 }
 
-func (std *Recursive) LookupNetIP(ctx context.Context, network, host string) (ips []net.IP, err error) {
+func (std *Recursive) LookupIP(ctx context.Context, network, host string) (ips []net.IP, err error) {
 	if network == "ip" || network == "ip4" {
 		ips, err = std.lookupNetIP(ctx, ips, host, dns.TypeA)
 	}
@@ -39,7 +40,7 @@ func (std *Recursive) LookupNetIP(ctx context.Context, network, host string) (ip
 
 func (std *Recursive) LookupHost(ctx context.Context, host string) (addrs []string, err error) {
 	var ips []net.IP
-	if ips, err = std.LookupNetIP(ctx, "ip", host); err == nil {
+	if ips, err = std.LookupIP(ctx, "ip", host); err == nil {
 		for _, ip := range ips {
 			addrs = append(addrs, ip.String())
 		}
@@ -47,9 +48,21 @@ func (std *Recursive) LookupHost(ctx context.Context, host string) (addrs []stri
 	return
 }
 
+func (std *Recursive) LookupNetIP(ctx context.Context, network, host string) (addrs []netip.Addr, err error) {
+	var ips []net.IP
+	if ips, err = std.LookupIP(ctx, "ip", host); err == nil {
+		for _, ip := range ips {
+			if ip, ok := netip.AddrFromSlice(ip); ok {
+				addrs = append(addrs, ip)
+			}
+		}
+	}
+	return
+}
+
 func (std *Recursive) LookupIPAddr(ctx context.Context, host string) (addrs []net.IPAddr, err error) {
 	var ips []net.IP
-	if ips, err = std.LookupNetIP(ctx, "ip", host); err == nil {
+	if ips, err = std.LookupIP(ctx, "ip", host); err == nil {
 		for _, ip := range ips {
 			addrs = append(addrs, net.IPAddr{IP: ip})
 		}
