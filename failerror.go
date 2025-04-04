c764ff6566af3d42ed196ba4bf4b359cd8eb5ab5
Johan Lindh <johan@linkdata.se>
2025-04-04 12:22:05 +0200

    wip

--- failerror.go
+++
@@ -1,7 +0,0 @@
-package recursive
-
-type failError struct{ e error }
-
-func (fe failError) Unwrap() error        { return fe.e }
-func (fe failError) Is(target error) bool { return target == ErrNoResponse }
-func (fe failError) Error() (s string)    { return fe.e.Error() }
