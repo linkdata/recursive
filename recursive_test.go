f257c7fee561c0cc9c6f8d6a16aec4e3fdeddd11
Johan Lindh <johan@linkdata.se>
2025-09-09 13:27:31 +0200

    refactor stub resolver setup

--- recursive_test.go
+++ recursive_test.go
@@ -4,26 +4,22 @@ import (
 	"context"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/miekg/dns"
 )
 
 func Test_Resolve1111(t *testing.T) {
 	DefaultCache = NewCache()
-	ipv4 := net.ParseIP("1.1.1.1")
-	aMsg := &dns.Msg{Answer: []dns.RR{&dns.A{
-		Hdr: dns.RR_Header{
-			Name:   "one.one.one.one.",
-			Rrtype: dns.TypeA,
-			Class:  dns.ClassINET,
-			Ttl:    60,
-		},
-		A: ipv4,
-	}}}
-	rec := newStubRecursive(map[uint16]*dns.Msg{dns.TypeA: aMsg})
+	var rec *Recursive
+	if networkAvailable() {
+		rec = New(nil)
+	} else {
+		rec = newStubResolver(stubResponses1111())
+	}
 	ctx := context.Background()
 
-	retv, srv, err := rec.dnsResolve(ctx, "one.one.one.one", dns.TypeA)
+	retv, srv, err := rec.DnsResolve(ctx, "one.one.one.one", dns.TypeA)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -33,6 +29,7 @@ func Test_Resolve1111(t *testing.T) {
 	if len(retv.Answer) == 0 {
 		t.Fatal("no Answer")
 	}
+	ipv4 := net.ParseIP("1.1.1.1")
 	foundit := false
 	for _, rr := range retv.Answer {
 		if a, ok := rr.(*dns.A); ok && a.A.Equal(ipv4) {
@@ -55,7 +52,7 @@ func Test_Resolve1111(t *testing.T) {
 	}
 
 	DefaultCache.DnsSet(retv)
-	msg := DefaultCache.DnsGet("one.one.one.one", dns.TypeA)
+	msg := DefaultCache.DnsGet("one.one.one.one.", dns.TypeA)
 	if msg == nil {
 		t.Fatal("expected cached message")
 	}
@@ -70,4 +67,13 @@ func Test_Resolve1111(t *testing.T) {
 	if hitratio == 0 {
 		t.Error("hit ratio is zero")
 	}
+}
+
+func networkAvailable() bool {
+	c, err := net.DialTimeout("tcp", "198.41.0.4:53", time.Second)
+	if err != nil {
+		return false
+	}
+	c.Close()
+	return true
 }
