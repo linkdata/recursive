5d95954c704b5b8c7822f89802b1f9040a58b1e8
Johan Lindh <johan@linkdata.se>
2025-09-11 12:55:48 +0200

    add test script

---
+++ testqueries.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+set -e
+trap 'echo "exit 0' INT
+$1 $2 $3 $4 $5 NS google.tw.cn.
+$1 $2 $3 $4 $5 NS bankgirot.nu.
+$1 $2 $3 $4 $5 NS skandia.com.ci.
+$1 $2 $3 $4 $5 A  m.hkirc.net.hk.
+$1 $2 $3 $4 $5 A  www.microsoft.com.
+$1 $2 $3 $4 $5 A  console.aws.amazon.com.
+$1 $2 $3 $4 $5 A  "*.en.se." 
+$1 $2 $3 $4 $5 A  teli.se.
+$1 $2 $3 $4 $5 A  telia.biz.mv.
+$1 $2 $3 $4 $5 A  telia.per.la.
+$1 $2 $3 $4 $5 NS seb.inf.ua
+$1 $2 $3 $4 $5 A  seb.org.tw
+$1 $2 $3 $4 $5 NS wetrgijrotigj.bet.ar
+$1 $2 $3 $4 $5 A  h6xyrckrof16xv31.xn--kprw13d
+$1 $2 $3 $4 $5 MX 3sj82qujmol2npax.us.kg
+$1 $2 $3 $4 $5 A  9ghuun5oshdr6hvi.prd.mg
+$1 $2 $3 $4 $5 NS 5xqy3o9qafuvdqtv.mil.sy
+$1 $2 $3 $4 $5 A  eoh.be
+$1 $2 $3 $4 $5 NS tella.net.ms
