90102d112522651d8db14a7df818f7a35cc5f8ce
Johan Lindh <johan@linkdata.se>
2025-09-17 12:50:33 +0200

    wip

--- testqueries.sh
+++ testqueries.sh
@@ -1,22 +1,21 @@
 #! /usr/bin/env bash
-set -e
 trap 'echo "exit 0' INT
-$1 $2 $3 $4 $5 NS google.tw.cn.
-$1 $2 $3 $4 $5 NS bankgirot.nu.
-$1 $2 $3 $4 $5 NS skandia.com.ci.
-$1 $2 $3 $4 $5 A  m.hkirc.net.hk.
-$1 $2 $3 $4 $5 A  www.microsoft.com.
-$1 $2 $3 $4 $5 A  console.aws.amazon.com.
-$1 $2 $3 $4 $5 A  "*.en.se." 
-$1 $2 $3 $4 $5 A  teli.se.
-$1 $2 $3 $4 $5 A  telia.biz.mv.
-$1 $2 $3 $4 $5 A  telia.per.la.
-$1 $2 $3 $4 $5 NS seb.inf.ua
-$1 $2 $3 $4 $5 A  seb.org.tw
-$1 $2 $3 $4 $5 NS wetrgijrotigj.bet.ar
-$1 $2 $3 $4 $5 A  h6xyrckrof16xv31.xn--kprw13d
-$1 $2 $3 $4 $5 MX 3sj82qujmol2npax.us.kg
-$1 $2 $3 $4 $5 A  9ghuun5oshdr6hvi.prd.mg
-$1 $2 $3 $4 $5 NS 5xqy3o9qafuvdqtv.mil.sy
-$1 $2 $3 $4 $5 A  eoh.be
-$1 $2 $3 $4 $5 NS tella.net.ms
+$1 $2 $3 $4 $5 NS google.tw.cn >  NS_google.tw.cn.txt
+$1 $2 $3 $4 $5 NS bankgirot.nu >  NS_bankgirot.nu.txt
+$1 $2 $3 $4 $5 NS skandia.com.ci >  NS_skandia.com.ci.txt
+$1 $2 $3 $4 $5 A m.hkirc.net.hk >  A_m.hkirc.net.hk.txt
+$1 $2 $3 $4 $5 A www.microsoft.com >  A_www.microsoft.com.txt
+$1 $2 $3 $4 $5 A console.aws.amazon.com >  A_console.aws.amazon.com.txt
+$1 $2 $3 $4 $5 A "*.en.se."  >  A_STAR.en.se.txt
+$1 $2 $3 $4 $5 A teli.se >  A_teli.se.txt
+$1 $2 $3 $4 $5 A telia.biz.mv >  A_telia.biz.mv.txt
+$1 $2 $3 $4 $5 A telia.per.la >  A_telia.per.la.txt
+$1 $2 $3 $4 $5 NS seb.inf.ua >  NS_seb.inf.ua.txt
+$1 $2 $3 $4 $5 A seb.org.tw >  A_seb.org.tw.txt
+$1 $2 $3 $4 $5 NS wetrgijrotigj.bet.ar >  NS_wetrgijrotigj.bet.ar.txt
+$1 $2 $3 $4 $5 A h6xyrckrof16xv31.xn--kprw13d >  A_h6xyrckrof16xv31.xn--kprw13d.txt
+$1 $2 $3 $4 $5 MX 3sj82qujmol2npax.us.kg >  MX_3sj82qujmol2npax.us.kg.txt
+$1 $2 $3 $4 $5 A 9ghuun5oshdr6hvi.prd.mg >  A_9ghuun5oshdr6hvi.prd.mg.txt
+$1 $2 $3 $4 $5 NS 5xqy3o9qafuvdqtv.mil.sy >  NS_5xqy3o9qafuvdqtv.mil.sy.txt
+$1 $2 $3 $4 $5 A eoh.be >  A_eoh.be.txt
+$1 $2 $3 $4 $5 NS tella.net.ms >  NS_tella.net.ms.txt
