2d558587f82e5c37b5b066375f045d9a885328a3
Johan Lindh <johan@linkdata.se>
2025-11-04 16:57:54 +0100

    simplify

--- ioutil.go
+++ ioutil.go
@@ -9,11 +9,7 @@ func readInt64(r io.Reader, numread *int64) (val int64, err error) {
 	b := [8]byte{}
 	var n int
 	if n, err = io.ReadFull(r, b[:]); err == nil {
-		err = io.ErrNoProgress
-		if n == 8 {
-			err = nil
-			val = int64(binary.BigEndian.Uint64(b[:])) //#nosec
-		}
+		val = int64(binary.BigEndian.Uint64(b[:])) //#nosec
 	}
 	*numread += int64(n)
 	return
@@ -23,12 +19,7 @@ func writeInt64(w io.Writer, written *int64, val int64) (err error) {
 	b := [8]byte{}
 	binary.BigEndian.PutUint64(b[:], uint64(val)) //#nosec
 	var n int
-	if n, err = w.Write(b[:]); err == nil {
-		err = io.ErrNoProgress
-		if n == 8 {
-			err = nil
-		}
-	}
+	n, err = w.Write(b[:])
 	*written += int64(n)
 	return
 }
@@ -37,11 +28,7 @@ func readUint16(r io.Reader, numread *int64) (val uint16, err error) {
 	b := [2]byte{}
 	var n int
 	if n, err = io.ReadFull(r, b[:]); err == nil {
-		err = io.ErrNoProgress
-		if n == 2 {
-			err = nil
-			val = uint16(binary.BigEndian.Uint16(b[:])) //#nosec
-		}
+		val = uint16(binary.BigEndian.Uint16(b[:])) //#nosec
 	}
 	*numread += int64(n)
 	return
@@ -51,12 +38,7 @@ func writeUint16(w io.Writer, written *int64, val uint16) (err error) {
 	b := [2]byte{}
 	binary.BigEndian.PutUint16(b[:], val) //#nosec
 	var n int
-	if n, err = w.Write(b[:]); err == nil {
-		err = io.ErrNoProgress
-		if n == 2 {
-			err = nil
-		}
-	}
+	n, err = w.Write(b[:])
 	*written += int64(n)
 	return
 }
