52a48c88278a8657fb4ed06918255351ee82a371
Johan Lindh <johan@linkdata.se>
2025-11-04 12:17:11 +0100

    nosec

--- cachevalue.go
+++ cachevalue.go
@@ -16,7 +16,7 @@ type cacheValue struct {
 func (cv *cacheValue) WriteTo(w io.Writer) (n int64, err error) {
 	var packed, b []byte
 	if packed, err = cv.Pack(); err == nil {
-		expiry := uint64(cv.expires.UnixMilli())
+		expiry := uint64(cv.expires.UnixMilli()) // #nosec G115
 		packlen := uint64(len(packed))
 		b = binary.BigEndian.AppendUint64(b, expiry)
 		b = binary.BigEndian.AppendUint64(b, packlen)
@@ -34,13 +34,13 @@ func (cv *cacheValue) ReadFrom(r io.Reader) (n int64, err error) {
 		n += 8
 		if err = binary.Read(r, binary.BigEndian, &packlen); err == nil {
 			n += 8
-			buf := make([]byte, int(packlen))
+			buf := make([]byte, int(packlen)) // #nosec G115
 			var numread int
 			if numread, err = r.Read(buf); err == nil {
 				var msg dns.Msg
 				if err = msg.Unpack(buf); err == nil {
 					cv.Msg = &msg
-					cv.expires = time.UnixMilli(int64(expiry))
+					cv.expires = time.UnixMilli(int64(expiry)) // #nosec G115
 				}
 			}
 			n += int64(numread)
